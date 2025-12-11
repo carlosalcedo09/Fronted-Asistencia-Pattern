a99471315e08b24ec819c37a45d9eb6d6f821c50
Carlos Alcedo <carlosalcedojavier@gmail.com>
2025-12-10 23:51:23 -0500

    ajust dev 11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export const Header = () => {
 
   const handleLogout = () => {
     localStorage.clear();
-    window.location.href = "/login";
+    window.location.href = "/";
   };
 
   return (
